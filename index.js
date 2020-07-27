ceeac4e4cf45ef56b7cb287fa2492e63ed01d1e1
Mihail Anton <mihail.anton@chalmers.se>
2020-07-27 11:14:30 +0200

    fix: cypher fulltext index

--- index.js
+++ index.js
@@ -732,7 +732,7 @@ try {
   `CALL db.index.fulltext.createNodeIndex(
     "fulltext",
     ["CompartmentState", "Compartment", "MetaboliteState", "Metabolite", "CompartmentalizedMetabolite", "SubsystemState", "Subsystem", "ReactionState", "Reaction", "GeneState", "Gene", "PubMedReference"],
-    ["id", "name", "letterCode", "alternateName", "synonyms", "description", "formula", "function", "pubMedID"]
+    ["id", "name", "letterCode", "alternateName", "synonyms", "description", "formula", "function", "pubMedID"])
   `.split('\n').forEach(i => {
     instructions.push(i);
   });
