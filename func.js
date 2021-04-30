c055ca08cd19d2b6ae9b1e55d1153d364eaf0154
nanjiangshu <nanjiang.shu@gmail.com>
2021-04-30 23:37:14 +0200

    refactor: better definition of the function trim

--- func.js
+++ func.js
@@ -1,15 +1,14 @@
-function trim(x, characters=" \t\w") {
-  var start = 0;
-  while (characters.indexOf(x[start]) >= 0) {
-    start += 1;
-  }
-  var end = x.length - 1;
-  while (characters.indexOf(x[end]) >= 0) {
-    end -= 1;
-  }
-  return x.substr(start, end - start + 1);
-}
-
 module.exports = {
-  trim,
+  trim: function (x, characters=" \t\w") {
+    var start = 0;
+    while (characters.indexOf(x[start]) >= 0) {
+      start += 1;
+    }
+    var end = x.length - 1;
+    while (characters.indexOf(x[end]) >= 0) {
+      end -= 1;
+    }
+    return x.substr(start, end - start + 1);
+  },
+
 }
