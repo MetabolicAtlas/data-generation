8f958b9d480151d48cbe693ce84d65529c9310aa
nanjiangshu <nanjiang.shu@gmail.com>
2021-05-12 13:17:13 +0200

    refactor: add files parser.js utils.js and writer.js that are responsible for specific tasks

---
+++ utils.js
@@ -0,0 +1,147 @@
+const fs = require('fs'), path = require('path');
+const parser = require('./parser.js');
+const trim = (x, characters=" \t\w") => {
+  var start = 0;
+  while (characters.indexOf(x[start]) >= 0) {
+    start += 1;
+  }
+  var end = x.length - 1;
+  while (characters.indexOf(x[end]) >= 0) {
+    end -= 1;
+  }
+  return x.substr(start, end - start + 1);
+}
+
+const cleanExternalId = (rawExternalId, dbName) => {
+  // clean rawExternalId
+  rawExternalId = trim(rawExternalId.trim(), '"');
+  if (dbName == 'MA'){
+    rawExternalId = rawExternalId.replace(/^MA-/, '');
+  } else if (dbName == 'ChEBI') {
+    rawExternalId = rawExternalId.replace(/^CHEBI:/, '');
+  } else if (dbName == 'Rhea' || dbName == 'RheaMaster') {
+    rawExternalId = rawExternalId.replace(/^RHEA:/, '');
+  }
+  return rawExternalId;
+}
+
+const getFile = (dirPath, regexpOrString) => {
+  if (!fs.existsSync(dirPath)){
+    console.log("no dir ", dirPath);
+    return;
+  }
+
+  const files = fs.readdirSync(dirPath);
+  for(let i = 0; i < files.length; i++) {
+    const filePath = path.join(dirPath, files[i]);
+    const stat = fs.lstatSync(filePath);
+    if (!stat.isDirectory() && (regexpOrString === files[i] || (regexpOrString.test && regexpOrString.test(files[i])))) {
+      return filePath;
+    }
+  }
+}
+
+
+const toLabelCase = (modelName) => {
+  return modelName.replace('-', ' ').split(/\s/g).map(word => `${word[0].toUpperCase()}${word.slice(1).toLowerCase()}`).join('');
+}
+
+const idfyString = s => s.toLowerCase().replace(/[^a-z0-9]/g, '_').replace(/_+/g, '_').replace(/^_|_$/, ''); // for subsystems, compartments etc..
+
+const idfyString2 = s => s.toLowerCase().replace(/[^a-z0-9]/g, '_').replace(/_+/g, '_'); // to generate compartmentalizedMetabolite ID from their name
+
+const mergedObjects = data => data.reduce((acc, item) => {
+  const [key, value] = Object.entries(item)[0];
+    return {
+    ...acc,
+        [key]: value,
+    };
+}, {});
+
+const reformatCompartmentObjets = (data) => {
+  return data.map((c) => {
+    name = Object.values(c)[0];
+    return { compartmentId: idfyString(name), name, letterCode: Object.keys(c)[0] };
+  });
+};
+
+const reformatGeneObjets = (data) => {
+  return data.map((g) => {
+    id = Object.values(g[0])[0];
+    return { geneId: id, name: '', alternateName: '', synonyms: '', function: '' };
+  });
+};
+
+const reformatCompartmentalizedMetaboliteObjets = (data) => {
+  return data.map((m) => {
+    m = mergedObjects(m);
+    return {
+      compartmentalizedMetaboliteId: m.id,
+      name: m.name,
+      alternateName: '',
+      synonyms: '',
+      description: '',
+      formula: m.formula,
+      charge: m.charge,
+      isCurrency: false,
+      compartment: m.compartment,
+    };
+  });
+};
+
+const reformatReactionObjets = (data) => {
+  return data.map((r) => {
+    // reactionId,name,reversible,lowerBound,upperBound,geneRule,ec
+    r = mergedObjects(r);
+    r.metabolites = mergedObjects(r.metabolites);
+    return {
+      reactionId: r.id,
+      name: r.name,
+      metabolites: r.metabolites,
+      lowerBound: r.lower_bound,
+      upperBound: r.upper_bound,
+      geneRule: r.gene_reaction_rule,
+      reversible: r.lower_bound === -1000,
+      ec: r.eccodes,
+      references: r.references,
+      subsystems: r.subsystem ? Array.isArray(r.subsystem) ? r.subsystem : [r.subsystem] : [],
+    };
+  } );
+};
+
+const getReactionRel = (content) => {
+  const reactionReactantRecords = [];
+  const reactionProductRecords = [];
+  const reactionGeneRecords = [];
+  const reactionSubsystemRecords = [];
+  content.reaction.forEach((r) => {
+    Object.entries(r.metabolites).forEach((e) => {
+      const [ compartmentalizedMetaboliteId, stoichiometry ] = e;
+      if (stoichiometry < 0) {
+        reactionReactantRecords.push({ compartmentalizedMetaboliteId, reactionId: r.reactionId, stoichiometry: -stoichiometry });
+      } else {
+        reactionProductRecords.push({ reactionId: r.reactionId, compartmentalizedMetaboliteId, stoichiometry });
+      }
+    });
+    parser.getGeneIdsFromGeneRule(r.geneRule).forEach((geneId) => {
+      reactionGeneRecords.push({ reactionId: r.reactionId, geneId });
+    });
+    r.subsystems.forEach((name) => {
+      reactionSubsystemRecords.push({ reactionId: r.reactionId, subsystemId: idfyString(name) });
+    })
+  });
+  return [reactionReactantRecords, reactionProductRecords, reactionGeneRecords, reactionSubsystemRecords];
+}
+
+
+exports.getFile = getFile;
+exports.toLabelCase = toLabelCase;
+exports.trim = trim;
+exports.cleanExternalId = cleanExternalId;
+exports.reformatGeneObjets = reformatGeneObjets;
+exports.reformatCompartmentObjets = reformatCompartmentObjets;
+exports.reformatCompartmentalizedMetaboliteObjets = reformatCompartmentalizedMetaboliteObjets;
+exports.reformatReactionObjets = reformatReactionObjets;
+exports.idfyString = idfyString;
+exports.idfyString2 = idfyString2;
+exports.getReactionRel = getReactionRel;
---
+++ writer.js
@@ -0,0 +1,206 @@
+const fs = require('fs'), path = require('path');
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+const utils = require('./utils.js');
+
+
+const writeComponentSvgCSV = (svgRels, outputPath, component) => {
+  const csvWriter = createCsvWriter({
+    path: `${outputPath}${component}SvgMaps.csv`,
+    header: [{ id: `${component}Id`, title: `${component}Id` },
+              { id: 'svgMapId', title: 'svgMapId' }],
+  });
+  csvWriter.writeRecords(svgRels);
+}
+
+const writePMIDCSV = (PMIDs, outputPath) => {
+  csvWriter = createCsvWriter({
+    path: `${outputPath}pubmedReferences.csv`,
+    header: [{ id: 'id', title: 'id' }],
+  });
+  csvWriter.writeRecords(PMIDs.map(
+    (id) => { return { id }; }
+  ));
+}
+const writeReactionPMIDCSV = (reactionPMID, outputPath) => {
+  csvWriter = createCsvWriter({
+    path: `${outputPath}reactionPubmedReferences.csv`,
+    header: [{ id: 'reactionId', title: 'reactionId' },
+             { id: 'pubmedReferenceId', title: 'pubmedReferenceId' }],
+  });
+  csvWriter.writeRecords(reactionPMID);
+}
+
+const writeComponentExternalDbCSV = (externalIdDBComponentRel, outputPath, fcomponent) => {
+  csvWriter = createCsvWriter({
+    path: `${outputPath}${fcomponent}ExternalDbs.csv`,
+    header: [{ id: `${fcomponent}Id`, title: `${fcomponent}Id` },
+              { id: 'externalDbId', title: 'externalDbId' }],
+  });
+  csvWriter.writeRecords(externalIdDBComponentRel.map(
+    (e) => { return { [`${fcomponent}Id`]: e.id, externalDbId: e.externalDbId }; }
+  ));
+}
+
+const writeSvgCSV = (svgNodes, outputPath) => {
+  csvWriter = createCsvWriter({
+    path: `${outputPath}svgMaps.csv`,
+    header: svgNodes.length ? Object.keys(svgNodes[0]).map(k => Object({ id: k, title: k })) : '',
+  });
+  csvWriter.writeRecords(svgNodes);
+}
+
+const writeExternalDbCSV = (externalIdNodes, outputPath) => {
+  csvWriter = createCsvWriter({
+    path: `${outputPath}externalDbs.csv`,
+    header: [{ id: 'id', title:'id' },
+             { id: 'dbName', title:'dbName' },
+             { id: 'externalId', title:'externalId' },
+             { id: 'url', title:'url' }],
+  });
+  csvWriter.writeRecords(externalIdNodes);
+}
+
+const writeMetaboliteCompartmentCSV = (content, outputPath) => {
+  const compartmentLetterToIdMap = content.compartment.reduce((entries, c) => {
+  // return a map which can get the compartment ID from the compartment letter
+    return {
+      ...entries,
+      [c.letterCode]: c.compartmentId,
+    };
+  }, {});
+
+  csvWriter = createCsvWriter({
+    path: `${outputPath}compartmentalizedMetaboliteCompartments.csv`,
+    header: [{ id: 'compartmentalizedMetaboliteId', title: 'compartmentalizedMetaboliteId' }, { id: 'compartmentId', title: 'compartmentId' }],
+  });
+
+  csvWriter.writeRecords(content.compartmentalizedMetabolite.map(
+    (e) => { return { compartmentalizedMetaboliteId: e.compartmentalizedMetaboliteId, compartmentId: compartmentLetterToIdMap[e.compartment] }; }
+  ));
+}
+
+const writeMetaboliteCSV = (content, outputPath) => {
+  csvWriter = createCsvWriter({
+    path: `${outputPath}compartmentalizedMetabolites.csv`,
+    header: [{ id: 'id', title: 'id' }],
+  });
+
+  csvWriter.writeRecords(content.compartmentalizedMetabolite.map(
+    (e) => { return { id: e.compartmentalizedMetaboliteId }; }
+  ));
+}
+
+const writeMetaboliteMetaboliteRelCSV = (content, uniqueCompartmentalizedMap, outputPath) => {
+  csvWriter = createCsvWriter({
+    path: `${outputPath}compartmentalizedMetaboliteMetabolites.csv`,
+    header: [{ id: 'compartmentalizedMetaboliteId', title: 'compartmentalizedMetaboliteId' }, { id: 'metaboliteId', title: 'metaboliteId' }],
+  });
+
+  csvWriter.writeRecords(content.compartmentalizedMetabolite.map(
+    (e) => { 
+      return { compartmentalizedMetaboliteId: e.compartmentalizedMetaboliteId,
+               metaboliteId: uniqueCompartmentalizedMap[e.compartmentalizedMetaboliteId] }; }
+  ));
+
+}
+
+const writeRRCSV = (reactionReactantRecords, outputPath) => {
+  // write reaction-reactants relationship files
+  csvWriterRR = createCsvWriter({
+    path: `${outputPath}compartmentalizedMetaboliteReactions.csv`,
+    header: [{ id: 'compartmentalizedMetaboliteId', title: 'compartmentalizedMetaboliteId' },
+             { id: 'reactionId', title: 'reactionId' },
+             { id: 'stoichiometry', title: 'stoichiometry' }],
+  });
+  csvWriterRR.writeRecords(reactionReactantRecords);
+}
+const writeRPCSV = (reactionProductRecords, outputPath) => {
+  // write reaction-products relationship files
+  csvWriterRP = createCsvWriter({
+    path: `${outputPath}reactionCompartmentalizedMetabolites.csv`,
+    header: [{ id: 'reactionId', title: 'reactionId' },
+             { id: 'compartmentalizedMetaboliteId', title: 'compartmentalizedMetaboliteId' },
+             { id: 'stoichiometry', title: 'stoichiometry' }],
+  });
+  csvWriterRP.writeRecords(reactionProductRecords);
+}
+const writeRGCSV = (reactionGeneRecords, outputPath) => {
+  // write reaction-genes relationship files
+  csvWriterRG = createCsvWriter({
+    path: `${outputPath}reactionGenes.csv`,
+    header: [{ id: 'reactionId', title: 'reactionId' },
+             { id: 'geneId', title: 'geneId' }],
+  });
+  csvWriterRG.writeRecords(reactionGeneRecords);
+}
+const writeRSCSV = (reactionSubsystemRecords, outputPath) => {
+  // write reaction-subsystems relationship files
+  csvWriterRS = createCsvWriter({
+    path: `${outputPath}reactionSubsystems.csv`,
+    header: [{ id: 'reactionId', title: 'reactionId' },
+             { id: 'subsystemId', title: 'subsystemId' }],
+  });
+  csvWriterRS.writeRecords(reactionSubsystemRecords);
+}
+
+const writeComponentCSV = (content, k, outputPath) => {
+  const elements = content[k];
+  csvWriter = createCsvWriter({
+    path: `${outputPath}${k}s.csv`,
+    header: [Object({ id: 'id', title: 'id' })],
+  });
+  csvWriter.writeRecords(elements.map(e => Object({ id: e[`${k}Id`] })));
+}
+
+const writeComponentStateCSV = (content, k, outputPath) => {
+  const elements = content[k];
+  csvWriter = createCsvWriter({
+    path: `${outputPath}${k}States.csv`,
+    header: Object.keys(elements[0]).
+      // ignore some keys 'metabolites', 'subsystems' are in reactions, 'compartment' is in metabolite
+      filter(k => !['metabolites', 'subsystems', 'compartment'].includes(k)).
+      map(k => Object({ id: k, title: k })),
+  });
+  // destructure object to remove the keys
+  csvWriter.writeRecords(elements.map(({ subsystems, metabolites, compartment, ...e }) => e));
+}
+
+const writeCyperFile = (instructions, outDir) => {
+  fs.writeFileSync(`${outDir}/import.cypher`, instructions.join('\n'), 'utf8');
+}
+
+const writeHpaRnaJson = (humanGeneIdSet, inputDir, outDir) => {
+// write a smaller version of the hpa rna levels file, to send to the frontend
+// remove expressions of genes not in any human models parsed
+  if (!fs.existsSync(`${inputDir}/hpaRnaFull.json`)) {
+      throw new Error("HPA rna JSON file not found");
+  } else {
+    const hpaRnaExpressionJson = require(`${inputDir}/hpaRnaFull.json`);
+
+    Object.keys(hpaRnaExpressionJson.levels).forEach((geneId) => {
+      if (!humanGeneIdSet.has(geneId)) {
+        delete hpaRnaExpressionJson.levels[geneId];
+      }
+    });
+
+    const json_rna = JSON.stringify(hpaRnaExpressionJson);
+    fs.writeFileSync(`${outDir}/hpaRna.json`, json_rna);
+  }
+}
+exports.writeComponentSvgCSV = writeComponentSvgCSV;
+exports.writePMIDCSV = writePMIDCSV;
+exports.writeReactionPMIDCSV = writeReactionPMIDCSV;
+exports.writeComponentExternalDbCSV = writeComponentExternalDbCSV;
+exports.writeSvgCSV = writeSvgCSV;
+exports.writeExternalDbCSV = writeExternalDbCSV;
+exports.writeMetaboliteCompartmentCSV = writeMetaboliteCompartmentCSV;
+exports.writeMetaboliteCSV = writeMetaboliteCSV;
+exports.writeMetaboliteMetaboliteRelCSV = writeMetaboliteMetaboliteRelCSV;
+exports.writeRRCSV = writeRRCSV;
+exports.writeRPCSV = writeRPCSV;
+exports.writeRGCSV = writeRGCSV;
+exports.writeRSCSV = writeRSCSV;
+exports.writeComponentCSV = writeComponentCSV;
+exports.writeComponentStateCSV = writeComponentStateCSV;
+exports.writeCyperFile = writeCyperFile;
+exports.writeHpaRnaJson = writeHpaRnaJson;
